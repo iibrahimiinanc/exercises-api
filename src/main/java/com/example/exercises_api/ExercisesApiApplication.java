dcaee38a3b19ab6d4ff0c70833ec3e680629a245
İbrahim İnanç <ibrahim.inanc@gmail.com>
2025-12-01 13:57:07 +0300

    First commit

---
+++ src/main/java/com/example/exercises_api/ExercisesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercises_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercises_api/ExercisesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercises_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
